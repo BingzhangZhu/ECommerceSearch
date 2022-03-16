03a2d05bad51e0d9888b6e8f03773f17cf01fae6
Bingzhang Zhu <bingzhangzhu@outlook.com>
2022-03-15 23:45:55 -0400

    finished task1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ECommerceSearch
+1. 首先进行jieba分词 参考文档： https://github.com/fxsjy/jieba
+2. 之后对中文进行TFIDF编码 参考文档： https://blog.csdn.net/blmoistawinde/article/details/80816179
--- code/data_explore.py
+++
@@ -1,9 +0,0 @@
-import jieba
-import pandas as pd
-
-corpus = pd.read_csv("../data/corpus.tsv", sep="\t", names = ["doc_id", "title"])
-corpus_head = corpus.head()
-corpus_head.loc[:, "seg_list"] = corpus_head.apply(lambda x: list(jieba.cut_for_search(x["title"])), axis=1)
-print(corpus_head.head())
-
-# print(list(jieba.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")))
